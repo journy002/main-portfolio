f28250b96b9e2ebc89920d947d4d7f5cc2e4915b
journy002 <journy002@naver.com>
2022-01-20 00:10:09 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import "../scss/style.scss";
+
+const nav = document.querySelector(".navbar");
+
+nav.addEventListener("click", (event) => {
+  const target = event.target;
+  const data = target.dataset.type;
+  scrollInto(data);
+});
+
+function scrollInto(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
