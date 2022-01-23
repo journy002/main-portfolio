56acfe2ef5081db58c3d975ff871d277935ab92f
journy002 <journy002@naver.com>
2022-01-23 16:12:59 +0900

    update

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ document.addEventListener("scroll", () => {
 
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
-  if (window.scrollY > homeHeight / 2) {
+  if (window.scrollY > homeHeight / 5) {
     arrowUp.classList.add("visible");
   } else {
     arrowUp.classList.remove("visible");
